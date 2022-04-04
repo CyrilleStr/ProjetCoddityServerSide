57375471b1baeb5b379bd48b3d6c7089ad24851d
Cyrille <cyrille.stroesser@orange.fr>
2022-04-05 00:41:36 +0200

    release1

---
+++ Procfile
@@ -0,0 +1,3 @@
+ web: gunicorn project_coddity_server_side.wsgi
+ release: python manage.py makemigrations --noinput
+ release: python manage.py migrate --noinput
--- project_coddity_server_side/settings.py
+++ project_coddity_server_side/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c^7g115**ij@%wc)p$vil64#zyiep00&b*v(xqz78@l1h^!g%r'
+SECRET_KEY = os.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
