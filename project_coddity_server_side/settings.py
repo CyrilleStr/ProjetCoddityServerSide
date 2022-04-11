935d9ef76572c13a4b5558ee13084bcd17a4c697
Cyrille <cyrille.stroesser@orange.fr>
2022-04-11 18:52:05 +0200

    remove staticfiles

--- project_coddity_server_side/settings.py
+++ project_coddity_server_side/settings.py
@@ -53,7 +53,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project_coddity_server_side.urls'
