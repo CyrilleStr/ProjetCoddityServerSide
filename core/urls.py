d214136643c6e36e2ee6b44cafcac0c9f731dc6a
Cyrille <cyrille.stroesser@orange.fr>
2022-06-11 08:26:01 +0200

    better auth response

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from . import views 
+from . import views
 
 urlpatterns = [
     path('register/', views.UserCreate.as_view()),
-    path('token/', obtain_auth_token),
+    path('token/', views.CustomAuthToken.as_view()),
     path('list/',views.UserList.as_view()),
     path('example/',views.ExampleView.as_view())
 ]
--- core/views.py
+++ core/views.py
@@ -7,6 +7,10 @@ from rest_framework.permissions import BasePermission, IsAuthenticated, AllowAny
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 
 class ExampleView(APIView):
     permission_classes = [IsAuthenticated]
@@ -17,6 +21,10 @@ class ExampleView(APIView):
             'auth': str(request.auth),  # None
         }
         return Response(content)
+
+class UserLogin(generics.GenericAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
 
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -29,3 +37,14 @@ class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk
+        })
